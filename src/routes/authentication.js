93cf9b170120adeaed3f0473c2c0c18b166f365f
William <williamhuchim@gmail.com>
2020-07-08 14:12:50 -0500

    primer commit

---
+++ src/routes/authentication.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+
+const passport = require('passport');
+const { isLoggedIn, isNotloggedIn } = require('../lib/auth')
+
+router.get('/signup', isNotloggedIn, (req, res) => {
+    res.render('auth/signup');
+})
+
+router.post('/signup', isNotloggedIn, passport.authenticate('local.signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/signup',
+    failureFlash: true
+}))
+
+router.get('/signin', isNotloggedIn, (req, res) => {
+    res.render('auth/signin');
+})
+
+router.post('/signin', isNotloggedIn, (req, res, next) => {
+    passport.authenticate('local.signin', {
+        successRedirect: '/profile',
+        failureRedirect: '/signin',
+        failureFlash: true
+    })(req, res, next);
+})
+
+router.get('/profile', isLoggedIn, (req, res) => {
+    res.render('profile');
+})
+
+router.get('/logout', isLoggedIn, (req, res) => {
+    req.logOut();
+    res.redirect('/');
+})
+
+module.exports = router;
